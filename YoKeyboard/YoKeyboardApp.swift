0266eed8a598392c839c226a53d0b24ef9c4605e
sbmsr <sebastian.messier@gmail.com>
2023-04-14 19:07:46 -0400

    Initial Commit

---
+++ YoKeyboard/YoKeyboardApp.swift
@@ -0,0 +1,17 @@
+//
+//  YoKeyboardApp.swift
+//  YoKeyboard
+//
+//  Created by Sebastian Messier on 4/14/23.
+//
+
+import SwiftUI
+
+@main
+struct YoKeyboardApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
